3e455b940eb3b1a6297a02ef856e4df75ed1cae5
walisilva <walison.eng@gmail.com>
2015-01-28 12:41:38 -0200

    Create index.php
    criado

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>:: TIPB ::</title>
+</head>
+<form action="config.php" method="post">
+<body>
+
+
+<input name="fone" type="text" value="83" size="20" maxlength="11" />
+
+<input name="enviar" type="submit" />
+
+<br />
+<br />
+
+
+Credits: <br />
+<a href="http://www.facebook.com/groups/tiparaibaa/" target="_blank">Grupo Ti - Paraíba<br /></a>
+
+
+
+</body>
+</form>
+</html>
